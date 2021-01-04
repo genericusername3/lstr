add611306a99691fa0a139fd1f8825b4c7af3873
Linus <6056695-linmathieu@users.noreply.gitlab.com>
2021-01-04 13:43:29 +0100

    Debugging will now be mentioned in opcua error messages

--- src/const.py
+++ src/const.py
@@ -1,4 +1,6 @@
 DEBUG = True
 CONNECTION_ERROR_TEXT = (
     "Die Liege wurde nicht erkannt. Ein Neustart könnte das Problem beheben."
+    if not DEBUG
+    else "Liege nicht erkannt. Der Debug-Modus ist angeschaltet."
 )
