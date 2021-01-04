67cd1c69a35e39fb899368f7ef371cc70f21854d
Linus <6056695-linmathieu@users.noreply.gitlab.com>
2021-01-04 13:44:05 +0100

    Update keyboard.py and keyboard.ui to include more special characters (ALT key added)

--- src/keyboard.py
+++ src/keyboard.py
@@ -71,6 +71,7 @@ class OnscreenKeyboard(Gtk.Grid):
     button_e02: Union[Gtk.Button, Gtk.Template.Child] = Gtk.Template.Child()
 
     shift_button: Union[Gtk.Button, Gtk.Template.Child] = Gtk.Template.Child()
+    alt_button: Union[Gtk.Button, Gtk.Template.Child] = Gtk.Template.Child()
     bksp_button: Union[Gtk.Button, Gtk.Template.Child] = Gtk.Template.Child()
 
     space_button: Union[Gtk.Button, Gtk.Template.Child] = Gtk.Template.Child()
@@ -79,6 +80,8 @@ class OnscreenKeyboard(Gtk.Grid):
 
     shift_active: bool = False
     shift_lock: bool = False
+
+    alt_active: bool = False
 
     shift_activation_time: float = 0
 
@@ -111,54 +114,362 @@ class OnscreenKeyboard(Gtk.Grid):
             return
 
         # Order of defining tuple:
-        # ((lowercase, label), (uppercase, label), (shiftlock, label))
+        # ((lowercase, label), (uppercase, label), (shiftlock, label), (alt + lowercase, label), (alt + uppercase, label), (alt + shiftlock, label))
         self.INSERT_KEYS: Dict[
             Gtk.Button, Tuple[Tuple[str, str], Tuple[str, str], Tuple[str, str]]
         ] = {
-            self.button_a00: (("1", "1"), ("!", "!"), ("1", "1")),
-            self.button_a01: (("2", "2"), ('"', '"'), ("2", "2")),
-            self.button_a02: (("3", "3"), ("§", "§"), ("3", "3")),
-            self.button_a03: (("4", "4"), ("$", "$"), ("4", "4")),
-            self.button_a04: (("5", "5"), ("%", "%"), ("5", "5")),
-            self.button_a05: (("6", "6"), ("&", "&"), ("6", "6")),
-            self.button_a06: (("7", "7"), ("/", "/"), ("7", "7")),
-            self.button_a07: (("8", "8"), ("(", "("), ("8", "8")),
-            self.button_a08: (("9", "9"), (")", ")"), ("9", "9")),
-            self.button_a09: (("0", "0"), ("=", "="), ("0", "0")),
-            self.button_a10: (("ß", "ß"), ("?", "?"), ("ẞ", "ẞ")),
-            self.button_b00: (("q", "q"), ("Q", "Q"), ("Q", "Q")),
-            self.button_b01: (("w", "w"), ("W", "W"), ("W", "W")),
-            self.button_b02: (("e", "e"), ("E", "E"), ("E", "E")),
-            self.button_b03: (("r", "r"), ("R", "R"), ("R", "R")),
-            self.button_b04: (("t", "t"), ("T", "T"), ("T", "T")),
-            self.button_b05: (("z", "z"), ("Z", "Z"), ("Z", "Z")),
-            self.button_b06: (("u", "u"), ("U", "U"), ("U", "U")),
-            self.button_b07: (("i", "i"), ("I", "I"), ("I", "I")),
-            self.button_b08: (("o", "o"), ("O", "O"), ("O", "O")),
-            self.button_b09: (("p", "p"), ("P", "P"), ("P", "P")),
-            self.button_b10: (("ü", "ü"), ("Ü", "Ü"), ("Ü", "Ü")),
-            self.button_c00: (("a", "a"), ("A", "A"), ("A", "A")),
-            self.button_c01: (("s", "s"), ("S", "S"), ("S", "S")),
-            self.button_c02: (("d", "d"), ("D", "D"), ("D", "D")),
-            self.button_c03: (("f", "f"), ("F", "F"), ("F", "F")),
-            self.button_c04: (("g", "g"), ("G", "G"), ("G", "G")),
-            self.button_c05: (("h", "h"), ("H", "H"), ("H", "H")),
-            self.button_c06: (("j", "j"), ("J", "J"), ("J", "J")),
-            self.button_c07: (("k", "k"), ("K", "K"), ("K", "K")),
-            self.button_c08: (("l", "l"), ("L", "L"), ("L", "L")),
-            self.button_c09: (("ö", "ö"), ("Ö", "Ö"), ("Ö", "Ö")),
-            self.button_c10: (("ä", "ä"), ("Ä", "Ä"), ("Ä", "Ä")),
-            self.button_d00: (("y", "y"), ("Y", "Y"), ("Y", "Y")),
-            self.button_d01: (("x", "x"), ("X", "X"), ("X", "X")),
-            self.button_d02: (("c", "c"), ("C", "C"), ("C", "C")),
-            self.button_d03: (("v", "v"), ("V", "V"), ("V", "V")),
-            self.button_d04: (("b", "b"), ("B", "B"), ("B", "B")),
-            self.button_d05: (("n", "n"), ("N", "N"), ("N", "N")),
-            self.button_d06: (("m", "m"), ("M", "M"), ("M", "M")),
-            self.button_e00: ((",", ","), (";", ";"), (",", ",")),
-            self.button_e01: ((".", "."), (":", ":"), (".", ".")),
-            self.button_e02: (("-", "-"), ("_", "_"), ("-", "-")),
-            self.space_button: ((" ", ""), (" ", ""), (" ", "")),
+            self.button_a00: (
+                ("1", "1"),
+                ("!", "!"),
+                ("1", "1"),
+                ("¡", "¡"),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a01: (
+                ("2", "2"),
+                ('"', '"'),
+                ("2", "2"),
+                ("'", "'"),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a02: (
+                ("3", "3"),
+                ("§", "§"),
+                ("3", "3"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a03: (
+                ("4", "4"),
+                ("$", "$"),
+                ("4", "4"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a04: (
+                ("5", "5"),
+                ("%", "%"),
+                ("5", "5"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a05: (
+                ("6", "6"),
+                ("&", "&"),
+                ("6", "6"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a06: (
+                ("7", "7"),
+                ("/", "/"),
+                ("7", "7"),
+                ("\\", "\\"),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a07: (
+                ("8", "8"),
+                ("(", "("),
+                ("8", "8"),
+                ("[", "["),
+                ("{", "{"),
+                ("{", "{"),
+            ),
+            self.button_a08: (
+                ("9", "9"),
+                (")", ")"),
+                ("9", "9"),
+                ("]", "]"),
+                ("}", "}"),
+                ("}", "}"),
+            ),
+            self.button_a09: (
+                ("0", "0"),
+                ("=", "="),
+                ("0", "0"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_a10: (
+                ("ß", "ß"),
+                ("?", "?"),
+                ("ẞ", "ẞ"),
+                ("\\", "\\"),
+                ("¿", "¿"),
+                ("¿", "¿"),
+            ),
+            self.button_b00: (
+                ("q", "q"),
+                ("Q", "Q"),
+                ("Q", "Q"),
+                ("@", "@"),
+                ("@", "@"),
+                ("@", "@"),
+            ),
+            self.button_b01: (
+                ("w", "w"),
+                ("W", "W"),
+                ("W", "W"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_b02: (
+                ("e", "e"),
+                ("E", "E"),
+                ("E", "E"),
+                ("€", "€"),
+                ("€", "€"),
+                ("€", "€"),
+            ),
+            self.button_b03: (
+                ("r", "r"),
+                ("R", "R"),
+                ("R", "R"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_b04: (
+                ("t", "t"),
+                ("T", "T"),
+                ("T", "T"),
+                ("þ", "þ"),
+                ("Þ", "Þ"),
+                ("Þ", "Þ"),
+            ),
+            self.button_b05: (
+                ("z", "z"),
+                ("Z", "Z"),
+                ("Z", "Z"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_b06: (
+                ("u", "u"),
+                ("U", "U"),
+                ("U", "U"),
+                ("ú", "ú"),
+                ("Ú", "Ú"),
+                ("Ú", "Ú"),
+            ),
+            self.button_b07: (
+                ("i", "i"),
+                ("I", "I"),
+                ("I", "I"),
+                ("í", "í"),
+                ("Í", "Í"),
+                ("Í", "Í"),
+            ),
+            self.button_b08: (
+                ("o", "o"),
+                ("O", "O"),
+                ("O", "O"),
+                ("ó", "ó"),
+                ("Ó", "Ó"),
+                ("Ó", "Ó"),
+            ),
+            self.button_b09: (
+                ("p", "p"),
+                ("P", "P"),
+                ("P", "P"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_b10: (
+                ("ü", "ü"),
+                ("Ü", "Ü"),
+                ("Ü", "Ü"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_c00: (
+                ("a", "a"),
+                ("A", "A"),
+                ("A", "A"),
+                ("á", "á"),
+                ("Á", "Á"),
+                ("Á", "Á"),
+            ),
+            self.button_c01: (
+                ("s", "s"),
+                ("S", "S"),
+                ("S", "S"),
+                ("ş", "ş"),
+                ("Ş", "Ş"),
+                ("Ş", "Ş"),
+            ),
+            self.button_c02: (
+                ("d", "d"),
+                ("D", "D"),
+                ("D", "D"),
+                ("ð", "ð"),
+                ("Ð", "Ð"),
+                ("Ð", "Ð"),
+            ),
+            self.button_c03: (
+                ("f", "f"),
+                ("F", "F"),
+                ("F", "F"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_c04: (
+                ("g", "g"),
+                ("G", "G"),
+                ("G", "G"),
+                ("ŋ", "ŋ"),
+                ("Ŋ", "Ŋ"),
+                ("Ŋ", "Ŋ"),
+            ),
+            self.button_c05: (
+                ("h", "h"),
+                ("H", "H"),
+                ("H", "H"),
+                ("ħ", "ħ"),
+                ("Ħ", "Ħ"),
+                ("Ħ", "Ħ"),
+            ),
+            self.button_c06: (
+                ("j", "j"),
+                ("J", "J"),
+                ("J", "J"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_c07: (
+                ("k", "k"),
+                ("K", "K"),
+                ("K", "K"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_c08: (
+                ("l", "l"),
+                ("L", "L"),
+                ("L", "L"),
+                ("ł", "ł"),
+                ("Ł", "Ł"),
+                ("Ł", "Ł"),
+            ),
+            self.button_c09: (
+                ("ö", "ö"),
+                ("Ö", "Ö"),
+                ("Ö", "Ö"),
+                ("ø", "ø"),
+                ("Ø", "Ø"),
+                ("Ø", "Ø"),
+            ),
+            self.button_c10: (
+                ("ä", "ä"),
+                ("Ä", "Ä"),
+                ("Ä", "Ä"),
+                ("å", "å"),
+                ("Å", "Å"),
+                ("Å", "Å"),
+            ),
+            self.button_d00: (
+                ("y", "y"),
+                ("Y", "Y"),
+                ("Y", "Y"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_d01: (
+                ("x", "x"),
+                ("X", "X"),
+                ("X", "X"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_d02: (
+                ("c", "c"),
+                ("C", "C"),
+                ("C", "C"),
+                ("ç", "ç"),
+                ("Ç", "Ç"),
+                ("Ç", "Ç"),
+            ),
+            self.button_d03: (
+                ("v", "v"),
+                ("V", "V"),
+                ("V", "V"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_d04: (
+                ("b", "b"),
+                ("B", "B"),
+                ("B", "B"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_d05: (
+                ("n", "n"),
+                ("N", "N"),
+                ("N", "N"),
+                ("ñ", "ñ"),
+                ("Ñ", "Ñ"),
+                ("Ñ", "Ñ"),
+            ),
+            self.button_d06: (
+                ("m", "m"),
+                ("M", "M"),
+                ("M", "M"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_e00: (
+                (",", ","),
+                (";", ";"),
+                (",", ","),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_e01: (
+                (".", "."),
+                (":", ":"),
+                (".", "."),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.button_e02: (
+                ("-", "-"),
+                ("_", "_"),
+                ("-", "-"),
+                ("", ""),
+                ("", ""),
+                ("", ""),
+            ),
+            self.space_button: (
+                (" ", " "),
+                (" ", " "),
+                (" ", " "),
+                (" ", " "),
+                (" ", " "),
+                (" ", " "),
+            ),
         }
 
         self.KEYS: Dict[Gtk.Button, Tuple[str, str, str]] = {
@@ -167,8 +478,9 @@ class OnscreenKeyboard(Gtk.Grid):
                 key: tuple([label for text, label in self.INSERT_KEYS[key]])
                 for key in self.INSERT_KEYS
             },
-            self.shift_button: ("⇧ Shift", "⇧ Shift", "⇪ Shift"),
-            self.bksp_button: ("⌫", "⌫", "⌫"),
+            self.alt_button: 6 * ("⌥ Alt",),
+            self.shift_button: 2 * ("⇧ Shift", "⇧ Shift", "⇪ Shift"),
+            self.bksp_button: 6 * ("⌫",),
         }
 
         for key in self.INSERT_KEYS:
@@ -177,6 +489,7 @@ class OnscreenKeyboard(Gtk.Grid):
         self.update_labels()
 
         self.shift_button.connect("clicked", self.on_shift_clicked)
+        self.alt_button.connect("clicked", self.on_alt_clicked)
         self.bksp_button.connect("button-press-event", self.on_bksp_pressed)
         self.bksp_button.connect("button-release-event", self.on_bksp_released)
 
@@ -194,12 +507,21 @@ class OnscreenKeyboard(Gtk.Grid):
         """
         insert_text: str
 
-        if self.shift_lock:
-            insert_text = self.INSERT_KEYS[button][2][0]
-        elif self.shift_active:
-            insert_text = self.INSERT_KEYS[button][1][0]
+        if self.alt_active:
+            if self.shift_lock:
+                insert_text = self.INSERT_KEYS[button][5][0]
+            elif self.shift_active:
+                insert_text = self.INSERT_KEYS[button][4][0]
+            else:
+                insert_text = self.INSERT_KEYS[button][3][0]
+
         else:
-            insert_text = self.INSERT_KEYS[button][0][0]
+            if self.shift_lock:
+                insert_text = self.INSERT_KEYS[button][2][0]
+            elif self.shift_active:
+                insert_text = self.INSERT_KEYS[button][1][0]
+            else:
+                insert_text = self.INSERT_KEYS[button][0][0]
 
         focus_widget: Gtk.Widget
         if self.dest_window is not None:
@@ -242,6 +564,22 @@ class OnscreenKeyboard(Gtk.Grid):
         else:
             button.get_style_context().remove_class("active")
             self.shift_active = False
+
+        self.update_labels()
+
+    def on_alt_clicked(self, button: Gtk.Button) -> None:
+        """React to a the alt button (key) being clicked.
+
+        Args:
+            button (Gtk.Button): The clicked button
+        """
+        if not self.alt_active:
+            button.get_style_context().add_class("active")
+            self.alt_active = True
+
+        else:
+            button.get_style_context().remove_class("active")
+            self.alt_active = False
 
         self.update_labels()
 
@@ -306,15 +644,33 @@ class OnscreenKeyboard(Gtk.Grid):
 
     def update_labels(self):
         """Update the key labels according to modifiers."""
-        if self.shift_lock:
-            for key in self.KEYS:
-                key.get_child().set_label(self.KEYS[key][2])
-        elif self.shift_active:
-            for key in self.KEYS:
-                key.get_child().set_label(self.KEYS[key][1])
+        if self.alt_active:
+            if self.shift_lock:
+                for key in self.KEYS:
+                    key.get_child().set_markup(self.KEYS[key][5].strip())
+                    key.set_sensitive(len(self.KEYS[key][5]) > 0)
+            elif self.shift_active:
+                for key in self.KEYS:
+                    key.get_child().set_markup(self.KEYS[key][4].strip())
+                    key.set_sensitive(len(self.KEYS[key][4]) > 0)
+            else:
+                for key in self.KEYS:
+                    key.get_child().set_markup(self.KEYS[key][3].strip())
+                    key.set_sensitive(len(self.KEYS[key][3]) > 0)
+
         else:
-            for key in self.KEYS:
-                key.get_child().set_label(self.KEYS[key][0])
+            if self.shift_lock:
+                for key in self.KEYS:
+                    key.get_child().set_markup(self.KEYS[key][2].strip())
+                    key.set_sensitive(len(self.KEYS[key][2]) > 0)
+            elif self.shift_active:
+                for key in self.KEYS:
+                    key.get_child().set_markup(self.KEYS[key][1].strip())
+                    key.set_sensitive(len(self.KEYS[key][1]) > 0)
+            else:
+                for key in self.KEYS:
+                    key.get_child().set_markup(self.KEYS[key][0].strip())
+                    key.set_sensitive(len(self.KEYS[key][0]) > 0)
 
 
 # Make OnscreenKeyboard accessible via .ui files
